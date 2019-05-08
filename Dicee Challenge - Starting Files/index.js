0603413c9863e7d80766cc3b9e3d41dba6099a31
Krista Scott <37915000+KristaScott@users.noreply.github.com>
2019-05-08 18:44:14 -0400

    Add files via upload

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -2,43 +2,13 @@ var randomNumber1 = Math.floor((Math.random() * 6) + 1);
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 
 // Dice #1 //
-if (randomNumber1 === 1) {
-  document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-}
-if (randomNumber1 === 2) {
-  document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-}
-if (randomNumber1 === 3) {
-  document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-}
-if (randomNumber1 === 4) {
-  document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-}
-if (randomNumber1 === 5) {
-  document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-}
-if (randomNumber1 === 6) {
-  document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-}
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
 // Dice #2 //
-if (randomNumber2 === 1) {
-  document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-}
-if (randomNumber2 === 2) {
-  document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-}
-if (randomNumber2 === 3) {
-  document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-}
-if (randomNumber2 === 4) {
-  document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-}
-if (randomNumber2 === 5) {
-  document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-}
-if (randomNumber2 === 6) {
-  document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-}
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
 // Message //
 
@@ -46,6 +16,6 @@ if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins!";
-} else if (randomNumber1 === randomNumber2) {
+} else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
